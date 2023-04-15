15d45bf781b8d0c8035e51e9b0d8c1396a13cf46
mihir224 <mihirsaini25@gmail.com>
2023-04-15 10:13:08 +0530

    initial commit - backend

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import questionRoutes from "./routes/questions.js";
+import resultRoutes from "./routes/results.js";
+
+//middleware
+const app=express();
+dotenv.config();
+const connect=()=>{
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("DB connected.") 
+    }).catch(err=>{ //throw err, if any
+        throw err;
+    });
+}
+app.use(express.json()); //data that we have to parse will be in the json format
+app.use(cors());
+app.use('/api/questions',questionRoutes);
+app.use('/api/results',resultRoutes);
+
+
+
+const port=process.env.PORT||5000;
+
+app.get('/',(req,res)=>{
+    res.send("hi");
+})
+
+app.listen(port,()=>{
+    connect();
+    console.log("server started on port " + port)
+})
